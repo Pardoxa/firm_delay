b15736f498bda2280f6fa7b1bc6cc687cbba6fc7
Yannick Feld <yannick.feld@uol.de>
2024-08-08 14:04:15 +0200

    major speed improvement

--- src/my_model/numeric_integration.rs
+++ src/my_model/numeric_integration.rs
@@ -1388,6 +1388,38 @@ fn master_ansatz_i_Ij_dependent(
 
     let mut Ii_given_prev_Ii = vec![vec![0.0; len]; len];
 
+    let aggregate = |Ij_t0: usize, other_k: usize|
+    {
+        let mut agg = vec![0.0; len];
+        for (Ij_t1, Ij_t0_prob) in Ij_given_prev_Ij[Ij_t0].iter().enumerate(){
+            let density = Ii_given_Ij_and_kij[Ij_t1].func[other_k].as_slice();
+            let outer_prob = Ij_t0_prob * bin_size2;
+            agg.iter_mut()
+                .zip(density)
+                .for_each(
+                    |(a,b)|
+                    {
+                        *a += b * outer_prob
+                    }
+                );
+        }
+        agg
+    };
+
+    let aggregated = (0..len)
+        .map(
+            |Ij_t0|
+            {
+                (0..idx_s)
+                    .map(
+                        |other_k: usize|
+                        {
+                            aggregate(Ij_t0, other_k)
+                        }
+                    ).collect_vec()
+            }
+        ).collect_vec();
+
     let mut Ii_given_prev_Ii_for_helper = |kij_tm1: usize, Ij_t0: usize, prob: f64|
     {
         let kIj_t0 = Ij_t0 + kij_tm1;
@@ -1398,18 +1430,16 @@ fn master_ansatz_i_Ij_dependent(
             let res_Ii_vec = Ii_given_prev_Ii[prev_Ii].as_mut_slice();
             let other_k = (kIj_t0 - prev_Ii).min(idx_s);
             if other_k < idx_s {
-                for (Ij_t1, Ij_t0_prob) in Ij_given_prev_Ij[Ij_t0].iter().enumerate(){
-                    let density = Ii_given_Ij_and_kij[Ij_t1].func[other_k].as_slice();
-                    let outer_prob = Ij_t0_prob * bin_size2 * prob;
-                    res_Ii_vec.iter_mut()
-                        .zip(density)
-                        .for_each(
-                            |(a,b)|
-                            {
-                                *a += b * outer_prob
-                            }
-                        );
-                }
+                let aggregate = aggregated[Ij_t0][other_k].as_slice();
+                res_Ii_vec
+                    .iter_mut()
+                    .zip(aggregate)
+                    .for_each(
+                        |(a, b)|
+                        {
+                            *a += b * prob
+                        }
+                    );
             } else {
                 // delta right
                 for (Ij_t1, Ij_t0_prob) in Ij_given_prev_Ij[Ij_t0].iter().enumerate(){
